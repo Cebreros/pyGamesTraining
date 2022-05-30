e9d912b80d0d8d4ded1313f32a72954a4c5943a6
Daniel Cebreros <cebreros2882@gmail.com>
2022-05-29 22:20:27 -0500

    adding images for ball and brick to finish lesson 18 in the course.

--- breakingBrigs.py
+++ breakingBrigs.py
@@ -1,16 +1,25 @@
 import pygame
-from pygame import image
-from pygame.constants import GL_MULTISAMPLEBUFFERS, QUIT
+#from pygame import image
+#from pygame.constants import GL_MULTISAMPLEBUFFERS, QUIT
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Breaking Bricks")
 
 #bat image loading
-bat = pygame.image.load("./imags/paddle.png")
+bat = pygame.image.load("./images/paddle.png")
 bat = bat.convert_alpha()
 bat_rect = bat.get_rect()
 
+#ball image loading
+ball = pygame.image.load("./images/football.png")
+ball = ball.convert_alpha()
+ball_rect = ball.get_rect()
+
+#brick image loading
+brick = pygame.image.load("./images/brick.png")
+brick = brick.convert_alpha()
+brick_rect = brick.get_rect()
 
 clock = pygame.time.Clock()
 game_over = False
