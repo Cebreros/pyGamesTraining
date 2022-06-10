a6cdcd67f01a40efecad9654b2d97f53f1ebe6c6
Daniel Cebreros <cebreros2882@gmail.com>
2022-06-09 20:26:32 -0500

    Add initial revision of Tetris

---
+++ Tetris/tetris.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((500,500))
+pygame.display.set_caption("Tetris")
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    screen.fill((0, 0, 0))
+    pygame.display.update()
+pygame.quit()
