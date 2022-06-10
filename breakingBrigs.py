83b825d07ec92c7b18eb33483e5c2a6f85fc55df
Daniel Cebreros <cebreros2882@gmail.com>
2022-06-09 20:12:07 -0500

    Code to remove bricks when are hit, lesson 23 section3

--- breakingBrigs.py
+++ breakingBrigs.py
@@ -82,6 +82,31 @@ while not game_over:
             sy *= 1.05 #increase the speed of ball in y
             continue
 
+    delete_brick = None
+    for b in bricks:
+        bx, by = b
+        if  bx <= ball_rect[0] <= bx + brick_rect.width and\
+            by <= ball_rect[1] <= by + brick_rect.height:
+            delete_brick = b
+
+            #change the direction of the ball when hit a brick
+            if ball_rect[0] <= bx + 2:
+                sx *= -1
+            elif ball_rect[0] >= bx +brick_rect.width - 2:
+                sx *= -1
+            
+            if ball_rect[1] <= by + 2:
+                sy *= -1
+            elif ball_rect[1] >= by + brick_rect.height -2:
+                sy *= -1
+            break
+        
+
+    
+    if delete_brick is not None:
+        bricks.remove(delete_brick)
+           
+
     #ball hit top
     if ball_rect[1] <= 0:
         ball_rect[1] = 0
